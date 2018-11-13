f70c0942d81bc80d059f2571485933b770db68e5
赵申侃 <zhaoshenkan@zhaoshenkandeMac-mini.local>
2018-11-13 17:22:50 +0800

    登录界面，点击事件

--- class/SKLogin.js
+++ class/SKLogin.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View,TextInput,TouchableOpacity,Text} from 'react-native';
 
+
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
     android:
@@ -26,20 +27,29 @@ export default class SKLogin extends Component<Props> {
                 >
                     <TouchableOpacity
                         style={styles.registBtn}
-                        onPress={this.registAccount()}
+                        onPress={()=> {this.registAccount()}}
                     >
-                        <Text> 免费注册 </Text>
+                        <Text style={styles.textStyle}>
+                            免费注册
+                        </Text>
                     </TouchableOpacity>
 
 
                     <TouchableOpacity
                         style={styles.forgetpwdBtn}
-                        onPress={this.forgetPwd()}
+                        onPress={()=> {this.forgetPwd()}}
                     >
-                        <Text> 忘记密码 </Text>
+                        <Text style={[styles.textStyle,styles.alignStyle]}> 忘记密码 </Text>
                     </TouchableOpacity>
 
                 </View>
+
+                <TouchableOpacity
+                    style={styles.login}
+                    onPress={()=>{this.doLogin()}}
+                >
+                    <Text style={styles.loginTitle}>登录</Text>
+                </TouchableOpacity>
 
 
             </View>
@@ -48,12 +58,40 @@ export default class SKLogin extends Component<Props> {
 
     registAccount(){
         alert("去注册账户")
+        // console.log('123')
     }
 
     forgetPwd(){
         alert('去修改密码')
     }
 
+
+    doLogin(){
+
+
+
+        fetch('https://xxxxxxxxxxxxxxxxxx', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify({
+                'userName': '15868192120',
+                'password': '123456',
+                'captcha': 'iDn2CDECMyb6z0IqPQlypAyKbkvqaEax',
+                'platform': '1',
+                'version': '1.0.0',
+            })
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                alert(responseJson.Message);
+                return responseJson.Message;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
 }
 
@@ -79,27 +117,52 @@ const styles = StyleSheet.create({
         height:40,
         borderBottomColor:'#000000',
         borderBottomWidth:0.8,
-
     },
 
     regist:{
         marginTop:5,
         height:30,
         flexDirection:'row',
-        backgroundColor:'#FF6EB4',
+        justifyContent: 'space-between',
     },
 
     registBtn:{
-        marginLeft:20,
         width:100,
         height:30,
+        marginLeft:20,
     },
 
     forgetpwdBtn:{
         marginRight:20,
         width:100,
         height:30,
-    }
+    },
+
+    textStyle:{
+        lineHeight:30,
+    },
+
+    alignStyle:{
+        textAlign: 'right',
+    },
+
+    login:{
+        backgroundColor: '#00FFFF',
+        height:40,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop:100,
+        borderRadius:4,
+
+
+    },
+
+    loginTitle:{
+        color:'#000000',
+        lineHeight: 40,
+        textAlign: 'center',
+
+    },
 
 
 
--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
-import SKLogin from './SKLogin';
+import SKLogin from './class/SKLogin';
 
 AppRegistry.registerComponent(appName, () => SKLogin);
